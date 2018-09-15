f36b92ed8107c703a0123ee7e14ba9fd91e8ff14
Steven Anderson <steven@sjanderson.org>
2018-09-14 20:21:21 -0700

    moar updates
    Signed-off-by: Steven Anderson <steven@sjanderson.org>

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,26 +1,14 @@
 jest.setTimeout(100000);
-const Cache = require('../lib/index.js');
 const request = require('supertest');
-const express = require('express');
 
-const cache = Cache({
-  disabled: false,
-});
-
-let app;
-
-beforeAll(() => {
-  app = express();
-  app.get('/', (req, res, next) => {
-    cache({ age: '5 minutes' }, (req, res, next) => {
-      next('hello world');
-    })(req, res, next);
-  });
-});
+const app = require('./server');
 
 describe('dobi-cache', () => {
   it('works as a cache', async () => {
     const resp = await request(app).get('/');
+    if (!['HIT', 'MISS'].includes(resp.headers['dobi-cache'])) {
+      console.error('run redis locally to test');
+    }
     expect(['HIT', 'MISS']).toContain(resp.headers['dobi-cache']);
   });
 
---
+++ tests/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const Cache = require('../lib/index');
+
+const cache = Cache({
+  disabled: false,
+  redisUri: 'localhost',
+});
+
+const app = express();
+app.get('/', (req, res, next) => {
+  cache({ age: '5 minutes' }, (_req, _res, _next) => {
+    _next('hello world');
+  })(req, res, next);
+});
+
+module.exports = app;
